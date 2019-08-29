8e2e1461da6a46bc47339fcc6cd4cc9107b387af
时间小鱼 <grantcolemans@gmail.com>
2019-08-29 16:33:18 +0800

    通过 Gitalk 增加评论功能

---
+++ docs/.vuepress/components/comment/comment.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="gitalk-container">
+    <div id="gitalk-container"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "comment",
+  data() {
+    return {};
+  },
+  mounted() {
+    let body = document.querySelector(".gitalk-container");
+    let script = document.createElement("script");
+    script.src = "https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js";
+    body.appendChild(script);
+    script.onload = () => {
+      const commentConfig = {
+        clientID: "a6b53533679a097b4df1",
+        clientSecret: "a741f69117f30107b5c10b8fa88781d8e1ec68b6",
+        repo: "Note",
+        owner: "G-Grant",
+        admin: ["G-Grant"],
+        id: location.pathname,
+        distractionFreeMode: false
+      };
+      const gitalk = new Gitalk(commentConfig);
+      gitalk.render("gitalk-container");
+    };
+  }
+};
+</script>
